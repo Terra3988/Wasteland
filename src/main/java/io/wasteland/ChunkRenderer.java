22e186c5ecc669ee06f7772a4f1f90ac90fc3f57
Terra3988 <kikfefshop@gmail.com>
2024-10-20 16:44:17 +0300

    Update
    Текстура в зависимости от стороны

--- src/main/java/io/wasteland/ChunkRenderer.java
+++ src/main/java/io/wasteland/ChunkRenderer.java
@@ -17,6 +17,33 @@ public class ChunkRenderer {
     }
 
     private void renderBlock(Tessellator tessellator, Chunk chunk, int x, int y, int z) {
+        // top
+        if(!chunk.isBlocked(x, y + 1, z)) {
+            renderBlockFace(tessellator, chunk, x, y, z, 0);
+        }
+        // bottom
+        if(!chunk.isBlocked(x, y - 1, z)) {
+            renderBlockFace(tessellator, chunk, x, y, z, 1);
+        }
+        // left
+        if(!chunk.isBlocked(x + 1, y, z)) {
+            renderBlockFace(tessellator, chunk, x, y, z, 2);
+        }
+        // right
+        if(!chunk.isBlocked(x - 1, y, z)) {
+            renderBlockFace(tessellator, chunk, x, y, z, 3);
+        }
+        // far
+        if(!chunk.isBlocked(x, y, z + 1)) {
+            renderBlockFace(tessellator, chunk, x, y, z, 5);
+        }
+        // near
+        if(!chunk.isBlocked(x, y, z - 1)) {
+            renderBlockFace(tessellator, chunk, x, y, z, 4);
+        }
+    }
+
+    private void renderBlockFace(Tessellator tessellator, Chunk chunk, int x, int y, int z, int face) {
 
         float x0 = x - 0.5f;
         float y0 = y - 0.5f;
@@ -26,13 +53,13 @@ public class ChunkRenderer {
         float y1 = y0 + 1f;
         float z1 = z0 + 1f;
 
-        float u0 = 0;
+        float u0 = face * (16.0f / 96.0f);
         float v0 = 0;
         float u1 = u0 + (16.0f / 96.0f);
         float v1 = 1;
 
         // top
-        if(!chunk.isBlocked(x, y + 1, z)) {
+        if(face == 0) {
             tessellator.vertex(x1, y1, z1, u0, v0);
             tessellator.vertex(x1, y1, z0, u0, v1);
             tessellator.vertex(x0, y1, z0, u1, v1);
@@ -42,7 +69,7 @@ public class ChunkRenderer {
             tessellator.vertex(x1, y1, z1, u0, v0);
         }
         // bottom
-        if(!chunk.isBlocked(x, y - 1, z)) {
+        if(face == 1) {
             tessellator.vertex(x0, y0, z1, u0, v0);
             tessellator.vertex(x0, y0, z0, u0, v1);
             tessellator.vertex(x1, y0, z0, u1, v1);
@@ -52,7 +79,7 @@ public class ChunkRenderer {
             tessellator.vertex(x0, y0, z1, u0, v0);
         }
         // left
-        if(!chunk.isBlocked(x + 1, y, z)) {
+        if(face == 2) {
             tessellator.vertex(x1, y1, z1, u0, v0);
             tessellator.vertex(x1, y0, z1, u0, v1);
             tessellator.vertex(x1, y0, z0, u1, v1);
@@ -62,7 +89,7 @@ public class ChunkRenderer {
             tessellator.vertex(x1, y1, z1, u0, v0);
         }
         // right
-        if(!chunk.isBlocked(x - 1, y, z)) {
+        if(face == 3) {
             tessellator.vertex(x0, y1, z0, u0, v0);
             tessellator.vertex(x0, y0, z0, u0, v1);
             tessellator.vertex(x0, y0, z1, u1, v1);
@@ -72,7 +99,7 @@ public class ChunkRenderer {
             tessellator.vertex(x0, y1, z0, u0, v0);
         }
         // far
-        if(!chunk.isBlocked(x, y, z + 1)) {
+        if(face == 5) {
             tessellator.vertex(x0, y1, z1, u0, v0);
             tessellator.vertex(x0, y0, z1, u0, v1);
             tessellator.vertex(x1, y0, z1, u1, v1);
@@ -82,7 +109,7 @@ public class ChunkRenderer {
             tessellator.vertex(x0, y1, z1, u0, v0);
         }
         // near
-        if(!chunk.isBlocked(x, y, z - 1)) {
+        if(face == 4) {
             tessellator.vertex(x1, y1, z0, u0, v0);
             tessellator.vertex(x1, y0, z0, u0, v1);
             tessellator.vertex(x0, y0, z0, u1, v1);
